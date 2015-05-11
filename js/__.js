7196fe5fee9afe9d0573aa1a60ecf986ee5681a1
Darren Liew <ldarren@gmail.com>
2015-05-11 08:54:17 +0000

    added ajax

--- js/__.js
+++ js/__.js
@@ -10,6 +10,51 @@ var __ = {
         }
         __.env.loaded = true
     },
+
+    // method: get/post, url: path, params: null/parameters (optional), headers: header parameter, cb: callback, userData: optional
+    ajax: function(method, url, params, headers, cb, userData){
+        cb = cb || dummyCB
+        if (!url) return cb(new Error('url not defined'))
+        var
+        xhr = XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP'),
+        post = 'POST' === (method = method.toUpperCase()),
+        dataType = ('string' === typeof params ? 1 : (params instanceof FormData ? 3 : 2))
+
+        url = encodeURI(url)
+
+        if (!post){
+            url += '?appVer='+__.env.appVer
+            if (params){
+                url += '&'
+                switch(dataType){
+                case 1: url += encodeURIComponent(params); break
+                case 2: url += Object.keys(params).reduce(function(a,k){a.push(k+'='+encodeURIComponent(params[k]));return a},[]).join('&'); break
+                case 3: return cb(new Error('FormData with GET method is not supported yet'))
+                }
+                params = null
+            }
+        }
+
+        xhr.open(method, url, true)
+
+        xhr.onreadystatechange=function(){
+            if (1 < xhr.readyState && cb){
+                var st = xhr.status
+                return cb((200 === st || !st) ? null : new Error("Error["+xhr.statusText+"] Info: "+xhr.responseText), xhr, userData)
+            }
+        }
+        xhr.onerror=function(evt){cb(evt, xhr, userData)}
+        // never set Content-Type, it will trigger preflight options and chrome 35 has problem with json type
+        //if (post && params && 2 === dataType) xhr.setRequestHeader('Content-Type', 'application/json')
+        if (post && params && 3 !== dataType) xhr.setRequestHeader('Content-Type', 'text/plain')
+        for (var key in headers) xhr.setRequestHeader(key, headers[key])
+
+        switch(dataType){
+        case 1: xhr.send(params); break
+        case 2: xhr.send(JSON.stringify(params)); break
+        case 3: xhr.send(params); break
+        }
+    },
 	createEvent: function(name, detail, bubbles, cancelable){
 		var evt = document.createEvent('CustomEvent')
 		evt.initCustomEvent(name, bubbles || false, cancelable || false, detail)
