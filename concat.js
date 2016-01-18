fd94acceb8875ee27b6f6a5271c301eccca0f563
Darren Liew <ldarren@gmail.com>
2016-01-18 16:30:14 +0000

    updated build tool

--- concat.js
+++ concat.js
@@ -5,7 +5,7 @@ fs = require('fs'),
 path = require('path'),
 uglify= require('uglify-js'),
 symPath = process.argv[1],
-dest = path.sep + (process.argv[2] || 'output.js'),
+dest = path.sep + (process.argv[2] || 'lean'),
 srcDir = path.sep + (process.argv[3] || 'js') + path.sep
 
 fs.readlink(symPath, function(err, realPath){
@@ -13,30 +13,30 @@ fs.readlink(symPath, function(err, realPath){
 	var wd = path.dirname(realPath)
 	dest = wd + dest
 	srcDir = wd + srcDir
-	console.log('delete', dest)
-	fs.unlink(dest, function(err){
+	console.log(`delete ${dest}.js`)
+	fs.unlink(dest+'.js', function(err){
 		console.log('read dir', srcDir)
 		fs.readdir(srcDir, function(err, files){
 			if (err) return console.error(err)
-			console.log('open file', dest)
-			var ws = fs.createWriteStream(dest, {flags:'a'});
+			console.log(`open file ${dest}.js`)
+			var ws = fs.createWriteStream(dest+'.js', {flags:'a'});
 			(function(cb){
 				if (!files.length) return cb()
 				var
 				fname = files.shift(),
 				callee = arguments.callee
 
-				if ('.' === fname[0]) return callee(cb)
+                if ('.'===path.basename(fname)[0]) return callee(cb)
 				console.log('appending', fname, '...')
 				var rs = fs.createReadStream(srcDir+fname)
 
 				rs.on('close', function(){ callee(cb) })
 				rs.pipe(ws, {end:false})
 			})(function(){
-                var min=uglify.minify(dest,{outSourceMap:dest+'.map'})
-                fs.writeFile(dest, min.code, 'utf8', (err)=>{
+                var min=uglify.minify(dest+'.js',{outSourceMap:dest+'.min.js.map'})
+                fs.writeFile(dest+'.min.js', min.code, 'utf8', (err)=>{
                     if (err) return console.error(err)
-                    fs.writeFile(dest+'.map', min.map, 'utf8', (err)=>{
+                    fs.writeFile(dest+'.min.js.map', min.map, 'utf8', (err)=>{
                         if (err) return console.error(err)
                         console.log('Done!')
                     })
