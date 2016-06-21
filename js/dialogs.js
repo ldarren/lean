4ecabd1f65266707bb0b540cb04da52585edeff0
Darren Liew <ldarren@gmail.com>
2016-06-21 10:00:24 +0000

    standardised prompt and confirm on cordova and browser

--- js/dialogs.js
+++ js/dialogs.js
@@ -1,18 +1,23 @@
 !function(){
-	__.dialogs={
-		alert:function(msg,title,btn,cb){ setTimeout(function(){alert(msg); (cb||__.dummyCB)()},0) },
-		confirm:function(msg,title,btns,cb){ setTimeout(function(){cb(null,confirm(msg))},0) },
-		prompt:function(msg,title,btns,comment,cb){ setTimeout(function(){cb(null,prompt(msg,comment))},0) },
-		beep:function(){console.log('beep')}
-	}
     function setup(){
 		var n=__.refChain(window,['navigator','notification'])
-		if (!n) return
-		__.dialogs={
-			alert:function(msg,title,btn,cb){ n.alert(msg,cb||__.dummyCB,title,btn) },
-			confirm:function(msg,title,btns,cb){ n.confirm(msg,cb,title,btns) },
-			prompt:function(msg,title,btns,comment,cb){ n.prompt(msg,cb,title,btns,confirms) },
-			beep:function(times){ n.beep(times) }
+		if (n) {
+			__.dialogs={
+				alert:function(msg,title,btn,cb){ n.alert(msg,cb||__.dummyCB,title,btn) },
+				confirm:function(msg,title,btns,cb){ n.confirm(msg,cb,title,btns) },
+				prompt:function(msg,title,btns,comment,cb){ n.prompt(msg,cb,title,btns,comment) },
+				beep:function(times){ n.beep(times) }
+			}
+		}else{
+			__.dialogs={
+				alert:function(msg,title,btn,cb){ setTimeout(function(){alert(msg); (cb||__.dummyCB)()},0) },
+				confirm:function(msg,title,btns,cb){ setTimeout(function(){cb(confirm(msg)?1:2)},0) },
+				prompt:function(msg,title,btns,comment,cb){ setTimeout(function(){
+					var m=prompt(msg,comment)
+					cb({buttonIndex:m?1:2,input1:m})
+				},0) },
+				beep:function(){console.log('beep')}
+			}
 		}
     }
 	__.onLoad(setup)
