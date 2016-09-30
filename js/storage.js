d0ce14581b166e1446e348a4ce75a621d8d5f2af
Darren Liew <ldarren@gmail.com>
2016-09-30 18:54:26 +0000

    fixed create table no res

--- js/storage.js
+++ js/storage.js
@@ -3,13 +3,14 @@
 		var
 		sp=__.refChain(window,['sqlitePlugin']),
 		od=__.refChain(window,['openDatabase']),
+		errCB=function(err){if(err)return console.error(err)},
 		db
 
 		if (sp || od){
 			if (sp) db=sp.openDatabase({name:'localstorage.db', location:'default'})
-			else db=openDatabase('localstorage.db', '1.0', 'fake local storage', 50 * 1024 * 1024)
+			else db=openDatabase('localstorage.db', '1.0', 'lean local storage emulator', 50 * 1024 * 1024)
 			db.transaction(function(tx){
-				tx.executeSql('CREATE TABLE IF NOT EXISTS kv (key TEXT UNIQUE NOT NULL, val TEXT);', function(tx,res){
+				tx.executeSql('CREATE TABLE IF NOT EXISTS kv (key TEXT UNIQUE NOT NULL, val TEXT);', null, function(tx,res){
 					console.log(res)
 				}, function(tx,err){
 					console.error(err)
@@ -18,7 +19,7 @@
 			__.storage={
 				key:function(idx,cb){
 					db.readTransaction(function(tx){
-						tx.executeSql('SELECT key FROM KV order by oid ASC;', [], function(tx,res){
+						tx.executeSql('SELECT key FROM KV order by oid ASC;', null, function(tx,res){
 							var rows=res.rows
 							if (rows.length <= idx) return cb()
 							cb(null,rows[idx].key)
@@ -38,6 +39,7 @@
 					})
 				},
 				setItem:function(key,val,cb){
+					cb=cb||errCB
 					db.transaction(function(tx){
 						tx.executeSql('INSERT OR REPLACE INTO kv (oid,key,val) VALUES ((SELECT oid FROM kv WHERE key=?), ?, ?);',
 						[key,key,val],
@@ -49,7 +51,7 @@
 					})
 				},
 				removeItem:function(key,cb){
-					cb=cb||__.dummyCB
+					cb=cb||errCB
 					db.transaction(function(tx){
 						tx.executeSql('DELETE FROM kv WHERE key=?;', [key], function(tx,res){
 							cb()
@@ -59,7 +61,7 @@
 					})
 				},
 				clear:function(cb){
-					cb=cb||__.dummyCB
+					cb=cb||errCB
 					db.transaction(function(tx){
 						tx.executeSql('DELETE FROM kv;',null,function(tx,res){
 							cb()
@@ -70,7 +72,7 @@
 				},
 				length:function(cb){
 					db.readTransaction(function(tx){
-						tx.executeSql('SELECT count(*) AS len FROM KV;', [], function(tx,res){
+						tx.executeSql('SELECT count(*) AS len FROM KV;', null, function(tx,res){
 							cb(null,res.rows[0].len)
 						}, function(tx,err){
 							cb(err)
@@ -84,16 +86,16 @@
 				key:function(index,cb){cb(null,db.key(key))},
 				getItem:function(key,cb){cb(null,db.getItem(key))},
 				setItem:function(key,val,cb){
-					cb=cb||__.dummyCB
+					cb=cb||errCB
 					try{cb(null,db.setItem(key,val))}
 					catch(exp){cb(exp)}
 				},
 				removeItem:function(key,cb){
-					cb=cb||__.dummyCB
+					cb=cb||errCB
 					cb(null,db.removeItem(key))
 				},
 				clear:function(cb){
-					cb=cb||__.dummyCB
+					cb=cb||errCB
 					cb(null,db.clear())
 				},
 				length:function(cb){ cb(null,db.length) }
