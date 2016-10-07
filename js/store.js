6290fb753ae1b7d137e847f39ba578b6a8e11c72
Darren Liew <ldarren@gmail.com>
2016-10-07 16:10:13 +0000

    allowed custom store creation

--- js/store.js
+++ js/store.js
@@ -4,19 +4,25 @@
 		sp=__.refChain(window,['sqlitePlugin']),
 		od=__.refChain(window,['openDatabase']),
 		errCB=function(err){if(err)return console.error(err)},
-		db
+		dbs={},
+		store
 
 		if (sp || od){
-			if (sp) db=sp.openDatabase({name:'localstorage.db', location:'default'})
-			else db=openDatabase('localstorage.db', '1.0', 'lean local storage emulator', 50 * 1024 * 1024)
-			db.transaction(function(tx){
-				tx.executeSql('CREATE TABLE IF NOT EXISTS kv (key TEXT UNIQUE NOT NULL, val TEXT);', null, null, function(tx,err){
-					console.error(err)
+			store=function(name){
+				var db
+				if (sp) db=sp.openDatabase({name:name, location:'default'})
+				else db=openDatabase(name, '1.0', 'lean local storage emulator', 50 * 1024 * 1024)
+				db.transaction(function(tx){
+					tx.executeSql('CREATE TABLE IF NOT EXISTS kv (key TEXT UNIQUE NOT NULL, val TEXT);',
+					null,
+					null,
+					function(tx,err){console.error(err)})
 				})
-			})
-			__.storage={
+				this.db=db
+			}
+			store.prototype={
 				key:function(idx,cb){
-					db.readTransaction(function(tx){
+					this.db.readTransaction(function(tx){
 						tx.executeSql('SELECT key FROM KV order by oid ASC;', null, function(tx,res){
 							var rows=res.rows
 							if (rows.length <= idx) return cb()
@@ -27,7 +33,7 @@
 					})
 				},
 				getItem:function(key,cb){
-					db.readTransaction(function(tx){
+					this.db.readTransaction(function(tx){
 						tx.executeSql('SELECT val FROM kv WHERE key=?;', [key], function(tx,res){
 							if (!res.rows.length) return cb()
 							cb(null,res.rows[0].val)
@@ -38,7 +44,7 @@
 				},
 				setItem:function(key,val,cb){
 					cb=cb||errCB
-					db.transaction(function(tx){
+					this.db.transaction(function(tx){
 						tx.executeSql('INSERT OR REPLACE INTO kv (oid,key,val) VALUES ((SELECT oid FROM kv WHERE key=?), ?, ?);',
 						[key,key,val],
 						function(tx,res){
@@ -50,7 +56,7 @@
 				},
 				removeItem:function(key,cb){
 					cb=cb||errCB
-					db.transaction(function(tx){
+					this.db.transaction(function(tx){
 						tx.executeSql('DELETE FROM kv WHERE key=?;', [key], function(tx,res){
 							cb()
 						}, function(tx,err){
@@ -60,7 +66,7 @@
 				},
 				clear:function(cb){
 					cb=cb||errCB
-					db.transaction(function(tx){
+					this.db.transaction(function(tx){
 						tx.executeSql('DELETE FROM kv;',null,function(tx,res){
 							cb()
 						},function(tx,err){
@@ -69,7 +75,7 @@
 					})
 				},
 				length:function(cb){
-					db.readTransaction(function(tx){
+					this.db.readTransaction(function(tx){
 						tx.executeSql('SELECT count(*) AS len FROM KV;', null, function(tx,res){
 							cb(null,res.rows[0].len)
 						}, function(tx,err){
@@ -79,25 +85,32 @@
 				}
 			}
 		}else{
-			db=window.localStorage
-			__.storage={
-				key:function(index,cb){cb(null,db.key(key))},
-				getItem:function(key,cb){cb(null,db.getItem(key))},
+			store=function(name){
+				this.prefix=name+'_'
+				this.db=window.localStorage
+			}
+			store.prototype={
+				key:function(index,cb){cb(null,this.db.key(index))},
+				getItem:function(key,cb){cb(null,this.db.getItem(this.prefix+key))},
 				setItem:function(key,val,cb){
 					cb=cb||errCB
-					try{cb(null,db.setItem(key,val))}
+					try{cb(null,this.db.setItem(this.prefix+key,val))}
 					catch(exp){cb(exp)}
 				},
 				removeItem:function(key,cb){
 					cb=cb||errCB
-					cb(null,db.removeItem(key))
+					cb(null,this.db.removeItem(this.prefix+key))
 				},
 				clear:function(cb){
 					cb=cb||errCB
-					cb(null,db.clear())
+					cb(null,this.db.clear())
 				},
-				length:function(cb){ cb(null,db.length) }
+				length:function(cb){ cb(null,this.db.length) }
 			}
+		}
+		__.store=function(name){
+			name=name||'localstorage'
+			return dbs[name] = dbs[name] || new store(name)
 		}
     }
 	__.onLoad(setup)
