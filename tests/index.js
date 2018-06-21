cbcfb392c6f4ec8b4d4a783bcf81999ee55af0ad
Darren Liew <ldarren@gmail.com>
2018-06-21 09:24:21 +0000

    added get ajax test

--- tests/index.js
+++ tests/index.js
@@ -5,19 +5,27 @@ ensure('ensure __.escapeXML work correctly', cb => {
 	const safe = __.escapeXML(unsafe)
 	cb(null, -1 === safe.indexOf('<script>'))
 })
-ensure('ensure __.ajax work correctly', cb => {
-	var xhr = __.ajax('POST', 'https://httpbin.org/post', 'WORK', null, (err, state, res) => {
+ensure('ensure __.ajax get work correctly', cb => {
+	const req = {hello: 'world'}
+	const xhr = __.ajax('GET', 'https://httpbin.org/get', req, null, (err, state, json) => {
 		if (4 !== state) return
-		cb(err, -1 !== res.indexOf('WORK'))
+		const res = JSON.parse(json)
+		cb(err, req.hello === res.args.hello)
+	})
+})
+ensure('ensure __.ajax post work correctly', cb => {
+	const req = {hello: 'world'}
+	const xhr = __.ajax('POST', 'https://httpbin.org/post', req, {headers:{'Content-Type':'application/json'}}, (err, state, json) => {
+		if (4 !== state) return
+		const res = JSON.parse(json)
+		cb(err, req.hello === res.json.hello)
 	})
 })
 ensure('ensure __.dom dataset work correctly', cb => {
-	var hello = 'world'
-	var body = __.dom.get({
+	const hello = 'world'
+	const body = __.dom.get({
 		el: 'body',
-		data: {
-			hello
-		}
+		data: { hello }
 	})
 
 	cb(null, hello === body.dataset.hello)
