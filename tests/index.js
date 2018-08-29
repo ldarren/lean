e50484d4954eeee9207e39b8a46427ead05a3af7
Darren Liew <ldarren@gmail.com>
2018-08-29 18:54:31 +0000

    updated test, added dom memory test

--- tests/index.js
+++ tests/index.js
@@ -1,11 +1,11 @@
-const { ensure } = pico.export('pico/test')
+const { test } = pico.export('pico/test')
 
-ensure('ensure __.escapeXML work correctly', cb => {
+test('ensure __.escapeXML work correctly', cb => {
 	const unsafe = "<script>alert('gotcha')</script>"
 	const safe = __.escapeXML(unsafe)
 	cb(null, -1 === safe.indexOf('<script>'))
 })
-ensure('ensure __.ajax get work correctly', cb => {
+test('ensure __.ajax get work correctly', cb => {
 	const req = {hello: 'world'}
 	const xhr = __.ajax('GET', 'https://httpbin.org/get', req, null, (err, state, json) => {
 		if (4 !== state) return
@@ -13,7 +13,7 @@ ensure('ensure __.ajax get work correctly', cb => {
 		cb(err, req.hello === res.args.hello)
 	})
 })
-ensure('ensure __.ajax post work correctly', cb => {
+test('ensure __.ajax post work correctly', cb => {
 	const req = {hello: 'world'}
 	const xhr = __.ajax('POST', 'https://httpbin.org/post', req, {headers:{'Content-Type':'application/json'}}, (err, state, json) => {
 		if (4 !== state) return
@@ -21,7 +21,7 @@ ensure('ensure __.ajax post work correctly', cb => {
 		cb(err, req.hello === res.json.hello)
 	})
 })
-ensure('ensure __.dom dataset work correctly', cb => {
+test('ensure __.dom dataset work correctly', cb => {
 	const hello = 'world'
 	const body = __.dom.get({
 		el: 'body',
