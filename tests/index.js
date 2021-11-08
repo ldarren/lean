65cb32b930d424a7331698c9b5a461b1dd499093
Darren Liew <ldarren@gmail.com>
2021-11-08 09:20:18 +0800

    Fix Ajax (#6)
    * removed excess urlencode
    
    * clean up qs generator
    
    * added formdata support

--- tests/index.js
+++ tests/index.js
@@ -5,6 +5,22 @@ test('ensure __.escapeXML work correctly', cb => {
 	const safe = __.escapeXML(unsafe)
 	cb(null, -1 === safe.indexOf('<script>'))
 })
+test('ensure __.formdata2json work correctly', cb => {
+	const formdata = new FormData
+	formdata.append('uid', 'hello')
+	formdata.append('passwd', 'world')
+	let json = __.formdata2json(formdata)
+	if (formdata.get('uid') !== json.uid || formdata.get('passwd') !== json.passwd) return cb(null, false)
+
+	const ele = document.createElement('form')
+	ele.innerHTML = `
+<label for="uid">name:</label><input type="text" name="uid" value="hello">
+<label for="passwd">password:</label><input type="text" name="passwd" value="world">
+<input type="submit" value="Submit!">
+  `
+	json = __.formdata2json(ele)
+	cb(null, formdata.get('uid') === json.uid && formdata.get('passwd') === json.passwd)
+})
 test('ensure __.ajax get work correctly', cb => {
 	const req = {i: '1'}
 	const xhr = __.ajax('GET', 'https://httpbin.org/get', req, null, (err, state, json) => {
@@ -27,6 +43,15 @@ test('ensure __.ajax post json work correctly', cb => {
 		if (4 !== state) return
 		const res = JSON.parse(json)
 		cb(err, req.i === res.json.i)
+	})
+})
+test('ensure __.ajax post formdata work correctly', cb => {
+	const ele = document.createElement('form')
+	ele.innerHTML = '<input type="text" name="i" value="1">'
+	const xhr = __.ajax('POST', 'https://httpbin.org/post', ele, {headers:{'Content-Type':'application/json'}}, (err, state, json) => {
+		if (4 !== state) return
+		const res = JSON.parse(json)
+		cb(err, (new FormData(ele)).get('i') === res.json.i)
 	})
 })
 test('ensure __.ajax get with opt.query', function(cb){
@@ -53,7 +78,7 @@ test('ensure __.ajax post with opt.query', function(cb){
 		cb(null, !args.q1 && args.q2 === '2')
 	})
 })
-test('ensure mxied query string works', function(cb){
+test('ensure mixed query string works', function(cb){
 	__.ajax('get', 'https://httpbin.org/anything?q1=1', {q2:2}, {query: {q3:3}}, (err,code,res)=>{
 		if (4!==code) return
 		if (err) return cb(err)
@@ -64,6 +89,18 @@ test('ensure mxied query string works', function(cb){
 		}
 		cb(null, args.q1 === '1' && args.q2 === '2' && args.q3 === '3')
 	})
+})
+test('ensure no over encodeURLComponent', function(cb){
+    __.ajax('get', 'https://httpbin.org/anything?<h1>=a,b', '<h2>=idx,id', {query: {'<h3>':'1,2,3'}}, (err,code,res)=>{
+        if (4!==code) return
+        if (err) return cb(err)
+        try{
+            var {args}=JSON.parse(res)
+        } catch(e){
+            cb(e)
+        }
+        cb(null, args['<h1>'] === 'a,b' && args['<h2>'] === 'idx,id' && args['<h3>'] === '1,2,3')
+    })
 })
 test('ensure __.dom dataset work correctly', cb => {
 	const hello = 'world'
